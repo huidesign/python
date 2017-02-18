8cd9846e18a3f60596a5338ac78eff752c7b5c12
zhanghui <comcan.zh@gmail.com>
2017-02-18 19:49:39 +0800

    first edition of a multithread crawler

---
+++ ToutiaoPlus/huiparse.py
@@ -0,0 +1,103 @@
+"""function used to parse web page"""
+import os
+import shutil
+import socket
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+# timeout in seconds
+TIMEOUT = 2
+socket.setdefaulttimeout(TIMEOUT)
+# file location
+FILE_LOCATION = "E:\\newphoto\\crazy"
+
+
+def generate_full_url(full_url, short_url):
+    """ generate new full url"""
+    url_list = list(full_url.rpartition('/'))
+    url_list[-1] = list(short_url.rpartition('/'))[-1]
+    return "".join(url_list)
+
+
+def generate_full_name(group_id, image_url, file_path):
+    """generate image full name"""
+    return os.path.join(file_path, group_id + "_" + image_url.rpartition('/')[-1])
+
+
+def strip_group_id(url):
+    """strip group id string from group url"""
+    return url.split('/')[-1].split('.')[0].split('_')[0]
+
+
+def download_work(pics):
+    """function do the download work, usually called by other modules"""
+    for pic in pics[1]:
+        download_pictures(pic, generate_full_name(pics[0], pic, FILE_LOCATION))
+
+
+def download_pictures(pic_url, file_name):
+    """picture downloads"""
+    class AppURLopener(urllib.request.FancyURLopener):
+        """dirive from FancyURLopenner"""
+        version = "Mozilla/5.0"
+
+    opener = AppURLopener()
+    print("Download: ", pic_url)
+    try:
+        response = opener.open(pic_url)
+        if response.closed:
+            return
+        out_file = open(file_name, 'wb')
+        shutil.copyfileobj(response, out_file)
+    except Exception as exception:
+        raise exception
+
+
+def deal_group_page(url):
+    """
+    deal with a new group page, recommend_pages returned
+    by this function should be further filtering
+
+    :param  recommend_pages [(groupID1, url1), (groupID2, url2)...]
+    :param  image_pages [url1, url2...]
+    :returns [(groupID1, url1), (groupID2, url2)...], (groupID, [url1, url2...])
+    """
+    recommend_pages = []
+    image_pages = []
+    next_pages = []  # next pages in current group page
+    next_url = url
+    while next_url not in next_pages:
+        next_pages.append(next_url)
+        next_url = deal_current_page(next_url, recommend_pages, image_pages)
+    return recommend_pages, (strip_group_id(url), image_pages)
+
+
+def deal_current_page(url, recommend_pages, image_pages):
+    """deal with web page"""
+    # open current page and parse with beautiful soup
+    html_doc = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html_doc, "lxml", from_encoding='GB18030')
+    # strip next page out from current page
+    next_page = generate_full_url(url, soup.find(id="photoNext")["href"])
+    # strip recommended urls out from current page
+    for tag in soup.find(class_="pic-list2 martop clearfix")("a"):
+        href = generate_full_url(url, tag["href"])
+        # be cautious, this href can be both short and long
+        if (strip_group_id(href), href) not in recommend_pages:
+            recommend_pages.append((strip_group_id(href), href))
+    # strip image urls out from current page
+    for tag in soup.find(id="big-pic").find_all("img"):
+        image_pages.append(tag["src"])
+
+    return next_page
+
+
+if __name__ == '__main__':
+    RECOMMONDS, PICS = deal_group_page("http://www.361games.com/html/tu/454956.html")
+    print("--------------------recommend_pages-----------------")
+    print(RECOMMONDS)
+    print("--------------------download_pages-----------------")
+    print(PICS)
+    for img in PICS[1]:
+        download_pictures(img, generate_full_name(PICS[0], img, FILE_LOCATION))
---
+++ ToutiaoPlus/test.py
@@ -0,0 +1,96 @@
+"""multithread spider"""
+import queue
+import threading
+from huiparse import deal_group_page
+from huiparse import download_work
+
+# max groups to traverse
+MAX_GROUPS_TO_TRAVERSE = 10
+# images to download : [(groupID1,[url1,url2,..])
+DOWNLOAD_QUEUE = queue.Queue()
+# groups to be traverse [(groupID1, url1), (groupID2, url2)...]
+TRAVERSE_QUEUE = queue.Queue()
+# groupID recorder to remember the groups that has been traversed
+GROUP_TRAVERSED = []
+# producer and consumer
+CONDITION = threading.Condition()
+# the start group page
+START_PAGE = "http://www.361games.com/html/tu/316376.html"
+
+
+def filter_with_shared_list(condition, share_list, elements):
+    """
+    filter the elements with shared list
+    :param share_list : [groupID1, groupID2...]
+    :param elements : [(groupID1, url1), (groupID2, url2)...]
+    """
+    if condition.acquire():
+        for item in elements:
+            if item[0] in share_list:
+                elements.remove(item)
+            else:
+                share_list.append(item[0])
+        condition.notify()
+        condition.release()
+
+
+def add_to_shared_list(condition, share_list, elements):
+    """add elements to shared list"""
+    if condition.acquire():
+        for item in elements:
+            if item not in share_list:
+                share_list.append(item)
+        condition.notify()
+        condition.release()
+
+
+def pop_from_shared_list(condition, share_list):
+    """pop element to shared list"""
+    if condition.acquire():
+        if len(share_list) > 0:
+            item = share_list.pop()
+        else:
+            item = None
+        condition.notify()
+        condition.release()
+        return item
+
+
+def group_page_process(start_page):
+    """the main proc tp process a new page"""
+    recommends, pics = deal_group_page(start_page)
+    # filter_with_shared_list(CONDITION, GROUP_TRAVERSED, recommends)
+    if len(recommends) != 0:
+        TRAVERSE_QUEUE.put(recommends)
+    if len(pics) != 0:
+        DOWNLOAD_QUEUE.put(pics)
+
+
+def producer(name):
+    """thread to parse the web page and generate the wanted contents"""
+    while True:
+        # if DOWNLOAD_QUEUE.qsize() <= 100:
+        next_groups = TRAVERSE_QUEUE.get()
+        print('Producer %s:' % name)
+        for item in next_groups:
+            group_page_process(item[1])
+
+
+def consumer(name):
+    """thread to do the downloading works"""
+    while True:
+        pics = DOWNLOAD_QUEUE.get()
+        print('Consumer %s:' % name)
+        download_work(pics)
+
+
+# the main function
+if __name__ == '__main__':
+    group_page_process(START_PAGE)
+    for i in range(1):
+        p = threading.Thread(target=producer, args=('zhanghui',))
+        p.start()
+
+    for i in range(4):
+        c = threading.Thread(target=consumer, args=('wenjing',))
+        c.start()
